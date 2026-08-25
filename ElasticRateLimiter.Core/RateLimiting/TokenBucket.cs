9e572d929db5e6898d719be2c3ac49c5d9798ccc
Manuel Mejia <me@manuelmejia.com>
2026-08-25 11:32:04 -0500

    Fix initialization of available tokens

--- ElasticRateLimiter.Core/RateLimiting/TokenBucket.cs
+++ ElasticRateLimiter.Core/RateLimiting/TokenBucket.cs
@@ -8,7 +8,7 @@ namespace ElasticRateLimiter.Core.RateLimiting
     public class TokenBucket(Func<int> clusterSizeProvider, long capacity, int refillRate, int reservedTokens = 0, bool isUnlimited = false)
     {
         internal readonly Lock SyncRoot = new();
-        private long _availableTokens;
+        private long _availableTokens = capacity > 0 ? Math.Max(1, capacity / (clusterSizeProvider ?? (() => 1))()) : 0;
         private DateTime _lastRefillUtc = DateTime.UtcNow;
 
         private readonly Func<int> _clusterSize = clusterSizeProvider ?? (() => 1);
